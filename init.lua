8c8a76774516d6264fc8b7d989104f735aaff7fc
menche <menche_mt@yahoo.com>
2012-11-15 17:43:32 -0800

    Initial commmit
    menche

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- External Command (external_cmd) mod by Menche
+-- Allows server commands / chat from outside minetest
+-- License: CC0
+
+minetest.register_globalstep(
+	function(dtime)
+		f = (io.open(minetest.get_modpath("external_cmd").."/message", "r"))
+		if f ~= nil then
+			local line = f:read("*line")
+			if line ~= nil then
+				minetest.chat_send_all("SERVER: "..line)
+			end
+			f:close()
+			os.remove(minetest.get_modpath("external_cmd").."/message")
+		end
+	end
+)
